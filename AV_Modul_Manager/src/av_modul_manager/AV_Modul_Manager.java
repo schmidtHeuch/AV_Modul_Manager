2d50825efb541a802fd16f10f97aa12812e64626
schmidtu <schmidtu@PC-352.heuch.local>
2018-03-05 07:54:09 +0100

    Neuanlage

---
+++ AV_Modul_Manager/src/av_modul_manager/AV_Modul_Manager.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package av_modul_manager;
+import UI.AV_MM_MainFrame;
+/**
+ *
+ * @author schmidtu
+ */
+public class AV_Modul_Manager {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        AV_MM_MainFrame av_mainFrame = new AV_MM_MainFrame();
+        av_mainFrame.setTitle("AV-Modul-Manager");
+        av_mainFrame.setSize(1024,900);
+        av_mainFrame.setVisible(true);      
+    }
+    
+}
